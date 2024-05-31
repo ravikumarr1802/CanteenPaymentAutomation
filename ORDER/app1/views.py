62c1c6ecf4ee739d1e0aae7e5265ebd20937fc68
NIKHIL <122862339+HELLNIX1@users.noreply.github.com>
2024-05-31 19:05:11 +0530

    Restart

--- ORDER/app1/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'temp/index.html')
-
-def contact(request):
-    method = request.method
-    return render(request,'temp/contact.html',{"method": method})
