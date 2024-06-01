4763b751c42d40e4ff245414155c1aa1b85d05d1
NIKHIL <122862339+HELLNIX1@users.noreply.github.com>
2024-06-01 09:37:41 +0530

    SECOND

--- order/customer/admin.py
+++ order/customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import MenuItem,Order,Category
 # Register your models here.
+
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(Category)
--- order/customer/views.py
+++ order/customer/views.py
@@ -8,4 +8,15 @@ class Index(View):
 class About(View):
     def get(self,request,*args,**kwargs):
         return render(request,'customer/base.html')
+    
+class Order(View):
+    def get(self,request,*args,**kwargs):
+        Tiffins = MenuItem.objects.filter(category_name_contains = 'Tiffin')
+        # Lunch = MenuItem.objects.filter(category_name_contains = 'Lunch')
+        # Fast Food = MenuItem.objects.filter(category_name_contains = 'Fast Food')
+
+        context = {
+            
+        }
+
 
